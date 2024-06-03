500df57cc1dff284285d508e3c713587f1b2133a
Aleksi Ojansivu <aleksi.ojansivu@gmail.com>
2024-06-03 20:11:33 +0300

    ex 8.1

--- index.js
+++ index.js
@@ -29,11 +29,13 @@ let authors = [
 
 /*
  * Suomi:
- * Saattaisi olla järkevämpää assosioida kirja ja sen tekijä tallettamalla kirjan yhteyteen tekijän nimen sijaan tekijän id
+ * Saattaisi olla järkevämpää assosioida kirja ja sen tekijä tallettamalla kirjan 
+ * yhteyteen tekijän nimen sijaan tekijän id
  * Yksinkertaisuuden vuoksi tallennamme kuitenkin kirjan yhteyteen tekijän nimen
  *
  * English:
- * It might make more sense to associate a book with its author by storing the author's id in the context of the book instead of the author's name
+ * It might make more sense to associate a book with its author by storing the author's 
+ * id in the context of the book instead of the author's name
  * However, for simplicity, we will store the author's name in connection with the book
  *
 */
@@ -90,19 +92,31 @@ let books = [
   },
 ]
 
-/*
-  you can remove the placeholder query once your first one has been implemented 
-*/
+
 
 const typeDefs = `
+  type Author {
+    name: String!,
+    id: ID!
+  }
+
+  type Book {
+    title: String!
+    published: Int!
+    author: Author!
+    genres: [String!]!
+  }
+
   type Query {
-    dummy: Int
+    bookCount: Int!
+    authorCount: Int!
   }
 `
 
 const resolvers = {
   Query: {
-    dummy: () => 0
+    bookCount: () => books.length,
+    authorCount: () => authors.length
   }
 }
 
